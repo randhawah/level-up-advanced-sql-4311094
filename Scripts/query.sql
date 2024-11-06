a04024eb9499c161984e6b2e071c26f8a2d3df85
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-06 18:34:44 +0000

    complete challange 3

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -11,9 +11,47 @@ INNER JOIN employee m
 
 -- Challange 2:
 -- Select name of sales person with 0 sales
-select e.employeeId, e.firstName, e.lastName, e.title, e.startDate, s.salesId
+select  e.firstName,
+        e.lastName,  
+        e.title, 
+        e.startDate, 
+        s.salesId
 from employee e
 left join sales s
 on e.employeeId = s.employeeId
 where e.title = 'Sales Person' and 
 e.employeeId not in (select employeeId from sales);
+
+-- Challange 3:
+-- get all data from customer table and sales table even if some of the
+-- data is missing because of the routine data cleanup jobs in place
+
+-- Sol: for this we need a full outter join, which is not compatible
+-- in SQLite we can create a inner join, left and right join then 
+-- combine all with a UNION to get the same results
+select  c.firstName, 
+        c.lastName, 
+        c.email,
+        s.salesAmount,
+        s.soldDate
+from customer c
+inner join sales s
+on c.customerId = s.customerId
+UNION
+select  c.firstName, 
+        c.lastName, 
+        c.email,
+        s.salesAmount,
+        s.soldDate
+from customer c
+left join sales s
+on c.customerId = s.customerId
+UNION
+select  c.firstName, 
+        c.lastName, 
+        c.email,
+        s.salesAmount,
+        s.soldDate
+from customer c
+right join sales s
+on c.customerId = s.customerId;
