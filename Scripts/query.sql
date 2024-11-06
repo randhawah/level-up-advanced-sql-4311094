9f42e868f9e9599f0c6ed5a6bab92aa1a4d714c2
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-06 22:10:59 +0000

    Complete all remainig challanges - mostly copy paste, these were hard!!

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -1,4 +1,4 @@
--- challange 1: 
+-- challange 1-1: 
 -- select name of employee and their manager
 SELECT  e.firstName,
         e.lastName,
@@ -9,7 +9,7 @@ FROM employee e
 INNER JOIN employee m
     ON e.managerId = m.employeeId;
 
--- Challange 2:
+-- Challange 1-2:
 -- Select name of sales person with 0 sales
 select  e.firstName,
         e.lastName,  
@@ -22,7 +22,7 @@ on e.employeeId = s.employeeId
 where e.title = 'Sales Person' and 
 e.employeeId not in (select employeeId from sales);
 
--- Challange 3:
+-- Challange 1-3:
 -- get all data from customer table and sales table even if some of the
 -- data is missing because of the routine data cleanup jobs in place
 
@@ -58,7 +58,7 @@ right join sales s
 on c.customerId = s.customerId
 where c.customerId is NULL;
 
--- Challange 4:
+-- Challange 2-1:
 -- pull a report that totals the number of cars sold by each employee
 
 select  e.employeeId, 
@@ -71,7 +71,7 @@ on e.employeeId = s.employeeId
 group by e.employeeId, e.firstName, e.lastName
 order by TotalSales DESC;
 
--- Challange 5:
+-- Challange 2-2:
 -- Find the least and most expensive car sold by each employee this year
 select  e.employeeId,
         e.firstName,
@@ -84,7 +84,7 @@ on e.employeeId = s.employeeId
 where s.soldDate between '2023-01-01' and '2023-12-31'
 group by e.employeeId;
 
--- Challange 6:
+-- Challange 2-3:
 -- Display report for employees who have sold at least 5 cars
 select  e.employeeId,
         e.firstName,
@@ -99,7 +99,7 @@ where s.soldDate between '2023-01-01' and '2023-12-31'
 group by e.employeeId, e.firstName, e.lastName
 having CarsSold > 5;
 
--- Challange 7:
+-- Challange 3-1:
 -- Summarise sales per year by using a CTE
 with cte as(
 select  strftime('%Y', soldDate) as year,
@@ -112,7 +112,7 @@ from cte
 group by year
 order by year;
 
--- Challange 8:
+-- Challange 3-2:
 -- Display cars sold for each employee by month for 2021
 -- rows per employee, with first name and last name, 
 -- columns with month names and values
@@ -149,10 +149,99 @@ where soldDate between '2021-01-01' and '2021-12-31'
 group by e.firstName, e.lastName
 order by e.lastName, e.firstName;
 
--- Challange 9:
+-- Challange 3-3:
 -- Find sales of cars which are electric by using a subquery
 
 select  s.soldDate, s.salesAmount, i.colour, i.year
 from sales s 
 join inventory i on s.inventoryId = i.inventoryId
 where i.modelId in (select modelId from model where EngineType = 'Electric');
+
+-- Challange 4-1:
+-- For each sales person rank the car models they've sold most
+
+-- First join the tables to get the necessary data
+SELECT emp.firstName, emp.lastName, mdl.model, sls.salesId
+FROM sales sls
+INNER JOIN employee emp
+  ON sls.employeeId = emp.employeeId
+INNER JOIN inventory inv
+  ON inv.inventoryId = sls.inventoryId
+INNER JOIN model mdl
+  ON mdl.modelId = inv.modelId
+
+-- apply the grouping
+SELECT emp.firstName, emp.lastName, mdl.model,
+  count(model) AS NumberSold
+FROM sales sls
+INNER JOIN employee emp
+  ON sls.employeeId = emp.employeeId
+INNER JOIN inventory inv
+  ON inv.inventoryId = sls.inventoryId
+INNER JOIN model mdl
+  ON mdl.modelId = inv.modelId
+GROUP BY emp.firstName, emp.lastName, mdl.model
+
+-- add in the windowing function
+SELECT emp.firstName, emp.lastName, mdl.model,
+  count(model) AS NumberSold,
+  rank() OVER (PARTITION BY sls.employeeId 
+              ORDER BY count(model) desc) AS Rank
+FROM sales sls
+INNER JOIN employee emp
+  ON sls.employeeId = emp.employeeId
+INNER JOIN inventory inv
+  ON inv.inventoryId = sls.inventoryId
+INNER JOIN model mdl
+  ON mdl.modelId = inv.modelId
+GROUP BY emp.firstName, emp.lastName, mdl.model
+
+-- Challange 4-2
+-- Create a report showing sales per month and an annual total
+
+-- get the needed data
+SELECT strftime('%Y', soldDate) AS soldYear, 
+  strftime('%m', soldDate) AS soldMonth, 
+  salesAmount
+FROM sales
+
+-- apply the grouping
+SELECT strftime('%Y', soldDate) AS soldYear, 
+  strftime('%m', soldDate) AS soldMonth,
+  SUM(salesAmount) AS salesAmount
+FROM sales
+GROUP BY soldYear, soldMonth
+ORDER BY soldYear, soldMonth
+
+-- add the window function - simplify with cte
+with cte_sales as (
+SELECT strftime('%Y', soldDate) AS soldYear, 
+  strftime('%m', soldDate) AS soldMonth,
+  SUM(salesAmount) AS salesAmount
+FROM sales
+GROUP BY soldYear, soldMonth
+)
+SELECT soldYear, soldMonth, salesAmount,
+  SUM(salesAmount) OVER (
+    PARTITION BY soldYear 
+    ORDER BY soldYear, soldMonth) AS AnnualSales_RunningTotal
+FROM cte_sales
+ORDER BY soldYear, soldMonth
+
+-- Challange 4-3
+-- Displays the number of cars sold this month, and last month
+
+-- Get the data
+SELECT strftime('%Y-%m', soldDate) AS MonthSold,
+  COUNT(*) AS NumberCarsSold
+FROM sales
+GROUP BY strftime('%Y-%m', soldDate)
+
+-- Apply the window function
+SELECT strftime('%Y-%m', soldDate) AS MonthSold,
+  COUNT(*) AS NumberCarsSold,
+  LAG (COUNT(*), 1, 0 ) OVER calMonth AS LastMonthCarsSold
+FROM sales
+GROUP BY strftime('%Y-%m', soldDate)
+WINDOW calMonth AS (ORDER BY strftime('%Y-%m', soldDate))
+ORDER BY strftime('%Y-%m', soldDate)
