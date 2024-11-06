f56848c9a69f641d0d48e6ddd59258448e86b08a
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2024-11-06 18:20:07 +0000

    complete challange 2

--- Scripts/query.sql
+++ Scripts/query.sql
@@ -1,4 +1,5 @@
-challange 1: 
+-- challange 1: 
+-- select name of employee and their manager
 SELECT  e.firstName,
         e.lastName,
         e.title,
@@ -6,6 +7,13 @@ SELECT  e.firstName,
         m.lastName AS ManagerLastName
 FROM employee e
 INNER JOIN employee m
-    ON e.managerId = m.employeeId
+    ON e.managerId = m.employeeId;
 
-Challange 2:
+-- Challange 2:
+-- Select name of sales person with 0 sales
+select e.employeeId, e.firstName, e.lastName, e.title, e.startDate, s.salesId
+from employee e
+left join sales s
+on e.employeeId = s.employeeId
+where e.title = 'Sales Person' and 
+e.employeeId not in (select employeeId from sales);
